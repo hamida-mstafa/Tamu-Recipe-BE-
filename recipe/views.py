ce87c7d84efa071e5b285e55106149215451410d
Lenny Makori <makorilenny28@gmail.com>
2020-06-23 11:36:24 +0300

    include POST request to serializer

--- recipe/views.py
+++ recipe/views.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from rest_framework.views  import APIView
 from django.contrib.auth.models import User
 from .serializer import UserSerializer
+from rest_framework import status
 
 # Create your views here.
 def index(request):
@@ -45,3 +46,10 @@ class UserList(APIView):
         # import pdb; pdb.set_trace()
         serializers = UserSerializer(all_users, many=True)
         return Response(serializers.data)
+
+    def post(self, request, format=None):
+        serializers = UserSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
