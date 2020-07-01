a7c4a4a7bcc5bb2c9367a04466d904b09cbd8df3
Beryl01 <berylnegesa@gmail.com>
2020-07-01 23:16:55 +0300

    Changes

--- recipe/urls.py
+++ recipe/urls.py
@@ -6,13 +6,21 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('api/user/', views.UserList.as_view()),
     path('api/profiles/' ,views.ProfileList.as_view()),
-    path('api/recipes/', views.RecipeList.as_view()),
+    #path('api/recipes/', views.RecipeList.as_view()),
+    path('api/ingredients/', views.IngredientList.as_view()),
     path('api/recipeingredients/', views.RecipeIngredientList.as_view()),
 
     path('api/user/user-id/<username>/',views.UserDetails.as_view()),
     path('api/profile/profile_id/<pk>/', views.Profiledetails.as_view()),
-    path('api/recipe/recipe_id/', views.Recipedetails.as_view()),
+    #path('api/recipe/recipe_id/', views.Recipedetails.as_view()),
     path('api/recipeingredient/recipeingredient_id/', views.RecipeIngredientdetails.as_view()),
+    path('api/ingredient/ingredient_id/', views.Ingredientdetails.as_view()),
+
+    path('api/recipes/', views.Recipes.as_view()),
+    path('api/recipes/name/<name>/', views.Recipes.as_view()),
+    path('api/recipes/country/<country>/', views.RecipesCountry.as_view()),
+    path('api/recipes/ingredients/<ingredients>/', views.RecipesIngredients.as_view()),
+    path('api/recipes/<pk>/', views.RecipeDetail.as_view()),
     
 ]
 
--- recipe/views.py
+++ recipe/views.py
@@ -9,10 +9,13 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import generics
 
 from .models import Profile, Recipe, RecipeIngredient
 from .serializer import (ProfileSerializer, RecipeSerializer,
                          RecipeIngredientSerializer, UserSerializer)
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
 class UserList(APIView):
@@ -54,119 +57,67 @@ class UserDetails(APIView):
         user.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-class ProfileList(APIView):
-    def get(self, request, format=None):
-        all_profiles = Profile.objects.all()
-        serializer = ProfileSerializer(all_profiles, many=True)
-        return Response(serializer.data)
+class ProfileList(generics.ListCreateAPIView):
+    # permission_classes = (IsAuthenticated)
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+        
 
-    def post(self, request, format=None):
-        serializer = ProfileSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_404_BAD_REQUEST) 
+class Profiledetails(generics.RetrieveUpdateDestroyAPIView):
+    # permission_classes = (IsAuthenticated)  
+    serializers = ProfileSerializer
+    queryset = Profile.objects.all()
 
-class Profiledetails(APIView):  
-    def getprofile(self, pk):
-        try:
-            return Profile.objects.get(pk=pk)
-        except Profile.DoesNotExist:
-            return Http404  
 
-    def get(self, request, pk, format=None):
-        profile = self.getprofile(pk)
-        serializers = ProfileSerializer(profile)
-        return Response(serializers.data)
+class IngredientList(generics.ListCreateAPIView):
+    # permission_classes = (IsAuthenticated)
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
 
-    def put(self, request, pk, format=None):
-        profile = self.getprofile(pk)
-        serializers = ProfileSerializer(profile, request.data)
-        if serializers.is_valid():
-            serializers.save()
-            return Response(serializers.data)
-        else:
-            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk, format=None):
-        profile = self.getprofile(pk)
-        profile.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)              
+class Ingredientdetails(generics.RetrieveUpdateDestroyAPIView):
+    # permission_classes = (IsAuthenticated)  
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+    lookup_field='name'
 
-class RecipeList(APIView):
-    def get(self, request, format=None):
-        all_recipes = Recipe.objects.all()
-        serializer = RecipeSerializer(all_recipes, many=True)
-        return Response(serializer.data) 
+class RecipeIngredientList(generics.ListCreateAPIView):
+    # permission_classes = (IsAuthenticated)
+    queryset = RecipeIngredient.objects.all()
+    serializer_class = RecipeIngredientSerializer
+     
+class RecipeIngredientdetails(generics.RetrieveUpdateDestroyAPIView):  
+    # permission_classes = (IsAuthenticated)
+    queryset = RecipeIngredient.objects.all()
+    serializer_class = RecipeIngredientSerializer
+    lookup_field = 'name'
 
-    def post(self, request, format=None):
-        serializer = RecipeSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_404_BAD_REQUEST) 
+class Recipes(generics.ListCreateAPIView):
+    serializer_class = RecipeSerializer
+    queryset = Recipe.objects.all()
+    filter_backends=[filters.SearchFilter]
+    search_fields = ['country','name','ingredients']
 
-class Recipedetails(APIView): 
-    def getrecipe(self, name):
-        try:
-            return Recipe.objects.get(name=name)
-        except Recipe.DoesNotExist:
-            return Http404  
+    def perform_create(self, serializer):
+        serializer.save(user=Profile.objects.first())
 
-    def get(self, request, format=None):
-        search_results=Recipe.objects.filter(name__icontains=self.request.query_params.get('name'))
-        res=serializers.serialize('json',search_results)
-        return Response(json.loads(res),status=status.HTTP_200_OK)
+class RecipeDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = RecipeSerializer
+    queryset = Recipe.objects.all()
 
-    def put(self, request, name, format=None):
-        recipe = self.getrecipe(name)
-        serializers = RecipeSerializer(recipe, request.data)
-        if serializers.is_valid():
-            serializers.save()
-            return Response(serializers.data)
-        else:
-            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+class RecipesCountry(generics.ListAPIView):
+    serializer_class = RecipeSerializer
+    queryset = Recipe.objects.all()
 
-    def delete(self, request, name, format=None):
-        recipe = self.getrecipe(name)
-        recipe.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)                 
+    def get_queryset(self):
+        country = self.kwargs['country']
+        return Recipe.objects.filter(country=country)
 
-class RecipeIngredientList(APIView):
-    def get(self, request, format=None):
-        all_recipeingredients = RecipeIngredient.objects.all()
-        serializer = RecipeIngredientSerializer(all_recipeingredients, many=True)
-        return Response(serializer.data)
+class RecipesIngredients(generics.ListAPIView):
+    serializer_class = RecipeSerializer
+    queryset = Recipe.objects.all()
 
-    def post(self, request, format=None):
-        serializer = RecipeIngredientSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_404_BAD_REQUEST) 
-
-class RecipeIngredientdetails(APIView):  
-    def getrecipeingredient(self, name):
-        try:
-            return RecipeIngredient.objects.get(name=name)
-        except RecipeIngredient.DoesNotExist:
-            return Http404  
-
-    def get(self, request, format=None):
-        search_results=RecipeIngredient.objects.filter(name__icontains=self.request.query_params.get('name'))
-        res=serializers.serialize('json',search_results)
-        return Response(json.loads(res),status=status.HTTP_200_OK)      
-
-    def put(self, request, name, format=None):
-        recipeingredient = self.getrecipeingredient(name)
-        serializers = RecipeIngredientSerializer(recipeingredient, request.data)
-        if serializers.is_valid():
-            serializers.save()
-            return Response(serializers.data)
-        else:
-            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, name, format=None):
-        recipeingredient = self.getrecipeingredient(name)
-        recipeingredient.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def get_queryset(self):
+        ingredients = self.kwargs['ingredients']
+        return Recipe.objects.filter(ingredients__icontains=ingredients)
+        
